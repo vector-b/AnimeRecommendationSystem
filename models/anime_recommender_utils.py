1c0621d46710fe82a01368e5d815a51633a7cba6
vector-b <==>
2023-07-25 22:59:24 -0300

    Fix Comments

--- models/anime_recommender_utils.py
+++ models/anime_recommender_utils.py
@@ -162,4 +162,123 @@ class UserRecommendation(AnimeRecommendation):
         self.user_pivot_table = pd.pivot_table(Rating_avg, values='avg_rating',index='user_id',columns='anime_id')
         self.user_rating = self.user_pivot_table.fillna(self.user_pivot_table.mean(axis=0))
 
-        Rating_avg
+        Rating_avg = Rating_avg.astype({"anime_id": str})
+        self.user_rating_ids = Rating_avg.groupby(by='user_id')['anime_id'].apply(lambda x:','.join(x))
+
+        self.cosine = self.get_general_similarity(self.user_rating)
+        np.fill_diagonal(self.cosine, 0)
+
+        self.square_rating = pd.DataFrame(self.cosine, index=self.user_rating.index)
+
+    # Get the most similar users based on cosine similarity
+    def get_most_similar_users(self, number):
+        df = self.square_rating.apply(lambda x: pd.Series(x.sort_values(ascending=False)
+           .iloc[:number].index, 
+          index=['{}'.format(i) for i in range(1, number+1)]), axis=1)
+        return df 
+
+    # Compute the user-item score for a given user and item
+    def user_item_score(self, user, item):
+
+        next_30_users = self.get_most_similar_users(15)
+        a = next_30_users[next_30_users.index==user].values
+
+        b = a.squeeze().tolist()
+        c = self.user_rating.loc[:,item]
+        d = c[c.index.isin(b)]
+
+        # Only the users with the same anime 
+        matched_users = d[d.notnull()]
+
+        index = matched_users.index.values.squeeze().tolist()
+
+        corr = self.square_rating.iloc[user, index]
+
+        # Create a DataFrame with the ratings and similarities
+        user_similarity_df = pd.concat([matched_users, corr], axis=1)
+        user_similarity_df.columns = ['avg_score', 'correlation']
+
+        # Calculate the final user score for the item using collaborative filtering formula
+        numerator = (user_similarity_df['avg_score'] * user_similarity_df['correlation']).sum()
+        denominator = user_similarity_df['correlation'].sum()
+
+        mean = self.mean
+    
+        avg_user = mean.loc[mean['user_id'] == user, 'rating'].values[0]
+
+        final_score = avg_user + (numerator / denominator)
+        return final_score
+    
+    # Get the set difference between two lists
+    def set_diff(self, list1, list2):
+        set_list1 = set(list1)
+        set_list2 = set(list2)
+
+        diff = list(set_list1 - set_list2)
+        return diff
+    
+    # Get a list of user-specific anime recommendations
+    def user_item_list_recommendation(self, user, number):
+        anime_seen_by_user = self.user_pivot_table.columns[self.user_pivot_table[self.user_pivot_table.index==user].notna().any()].tolist()
+        anime_seen_by_user = list(map(str, anime_seen_by_user))
+
+        next_30_users = self.get_most_similar_users(3)
+        a = next_30_users[next_30_users.index==user].values
+        b = a.squeeze().tolist()
+        d = self.user_rating_ids[self.user_rating_ids.index.isin(b)]
+        l = ','.join(d.values)
+        
+        anime_seen_by_similar_users = list(l.split(','))
+
+        animes_under_consideration = self.set_diff(anime_seen_by_similar_users, anime_seen_by_user)
+        animes_under_consideration = list(map(int, filter(lambda x: x.strip(), animes_under_consideration)))
+
+        score = []
+        for item in animes_under_consideration:
+            c = self.user_rating.loc[:,item]
+            d = c[c.index.isin(b)]
+            f = d[d.notnull()]
+            avg_user = self.mean.loc[self.mean['user_id'] == user,'rating'].values[0]
+            index = f.index.values.squeeze().tolist()
+            corr = self.square_rating.loc[user,index]
+
+            fin = pd.concat([f, corr], axis=1)
+            fin.columns = ['avg_score','correlation']
+            fin['score']=fin.apply(lambda x:x['avg_score'] * x['correlation'],axis=1)
+            nume = fin['score'].sum()
+            deno = fin['correlation'].sum()
+            final_score = avg_user + (nume/deno)
+            score.append(final_score)
+
+        data = pd.DataFrame({'anime_id':animes_under_consideration,'score':score})
+        top_recommendation = data.sort_values(by='score', ascending=False).head(number)
+        anime_Name = top_recommendation.merge(self.anime_complete, how='inner', left_on='anime_id', right_on='MAL_ID')
+        anime_name_list = anime_Name.Name.values.tolist()
+
+        anime_list = [{"nome": name, "descricao": self.get_anime_description_by_name(name)} for name in anime_name_list]
+
+        return anime_list
+
+    # Get the taste relation between a user and an anime
+    def get_user_taste_relation(self):
+        unique_users = self.unique_users
+        unique_anime = self.unique_anime
+        input_value_1 = st.selectbox(
+            'Selecione a id do usuário: ',
+            options=unique_users)
+
+        input_value_2 = st.selectbox(
+            'Selecione a id do anime: ',
+            options=unique_anime)
+        
+        st.write(f"Relação de gosto de usuários {self.user_item_score(input_value_1, input_value_2)}")
+
+    # Get a list of anime recommendations for a specific user
+    def get_user_list_recommendation(self):
+        unique_users = self.unique_users
+        input_value_1 = st.selectbox(
+            'Selecione a id do usuário: ',
+            options=unique_users)
+        input_value_2 = st.slider('Número de Animes: ', 1, 30, 10)
+
+        self.show_anime_list_expander(self.user_item_list_recommendation(input_value_1, input_value_2))
